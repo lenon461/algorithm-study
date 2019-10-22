5d8670b441c30fc39f378db66e232ff00d9fd731
livefrugally <livefrugally@naver.com>
2019-10-22 15:28:18 +0900

    add comments

--- 58. Length of Last Word/[livefrugally] 58. Length of Last Word.java
+++ 58. Length of Last Word/[livefrugally] 58. Length of Last Word.java
@@ -1,3 +1,6 @@
+//Runtime: 1 ms, faster than 46.74% of Java online submissions for Length of Last Word.
+//Memory Usage: 36.3 MB, less than 100.00% of Java online submissions for Length of Last Word.
+
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] arr = s.split(" ");
