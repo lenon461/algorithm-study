452c490e59a3e6f5af71b2e723a83dc75317f06c
Jis2nYoon <53715000+Jis2nYoon@users.noreply.github.com>
2019-10-23 11:08:08 +0900

    Create [jis2nYoon]1. Two Sum.java
    2019년 10월 4번째 주 문제

---
+++ 1. Two Sum/[jis2nYoon]1. Two Sum.java
@@ -0,0 +1,27 @@
+/*
+ * Runtime: 14 ms, faster than 43.31% of Java online submissions for Two Sum.
+ * Memory Usage: 36.5 MB, less than 99.48% of Java online submissions for Two Sum.
+ * 29 / 29 test cases passed.
+ * Runtime: 14 ms
+ * Memory Usage: 36.5 MB
+ */
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    
+    int[] answer = new int[2];
+    int num1;
+    int num2;
+    for (int i = 0; i < nums.length; i++) {
+      num1 = nums[i];
+      num2 = target - num1;
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[j] == num2) {
+          answer[0] = i;
+          answer[1] = j;
+          return answer;
+        }
+      }
+    }
+    return answer;
+  }
+}
