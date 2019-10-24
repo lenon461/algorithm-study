d86f31f6e8a6248d236a3c08facb24a166afbcf2
goneone <kuyt1819@gmail.com>
2019-10-24 11:07:39 +0900

    test

--- 1. Two Sum/[goneone]1.Two Sum.java
+++ 1. Two Sum/[goneone]1.Two Sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
-        //1.for문 돌려서 target 나오면 return i, j 값
+
         for(int i = 0; i<nums.length -1 ; i++) {
             for(int j = 1; j< nums.length ; j++) {
                 if(i != j) {
